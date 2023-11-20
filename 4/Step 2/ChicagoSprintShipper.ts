af579f9e0b07dc303e792e6ca8afd87325b9b404
Yuliia Varchenko <varchenkojulia1@gmail.com>
2023-11-20 17:21:17 +0200

    hw4 step 2

---
+++ 4/Step 2/ChicagoSprintShipper.ts
@@ -0,0 +1,5 @@
+import { Shipper } from "./Shipper";
+
+export class ChicagoSprintShipper extends Shipper {
+    public shipmentCost: number = 0.42;
+}
---
+++ 4/Step 2/PacificParcelShipper.ts
@@ -0,0 +1,5 @@
+import { Shipper } from "./Shipper";
+
+export class PacificParcelShipper extends Shipper {
+    public shipmentCost: number = 0.51;
+}
