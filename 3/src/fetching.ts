069cd5da475f656884db2b9b3e142d4e4da14c0b
Yuliia Varchenko <varchenkojulia1@gmail.com>
2023-11-28 17:23:08 +0200

    Revert "hw 3 initial"
    This reverts commit 5c2696740aef128c41d00bc963bde2da10213444.

--- 3/src/fetching.ts
+++
@@ -1,9 +0,0 @@
-import { mockClient, mockExecutor } from './mocks';
-
-const sleep = (ms: number) => new Promise((res) => setTimeout(res, ms));
-
-export const fetchExecutor = () => sleep(500)
-  .then(() => mockExecutor);
-
-export const fetchClient = () => sleep(700)
-  .then(() => mockClient);
